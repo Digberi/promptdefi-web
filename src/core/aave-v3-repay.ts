378ebf94724c62aa99c97123448f13e693851426
KStasi <nastasiakondaurova@gmail.com>
2023-06-02 12:33:29 +0300

    support basic aave v3 actions

---
+++ src/core/aave-v3-repay.ts
@@ -0,0 +1,36 @@
+import { BigNumberish, utils } from 'ethers';
+
+import tokenABI from '@/config/abi/erc20.abi.json';
+import aaveV3PoolABI from '@/config/abi/aave-v3-pool.abi.json';
+
+const AAVE_V3_POOL_CONTRACT_ADDRESS = '0x7b5C526B7F8dfdff278b4a3e045083FBA4028790';
+const INTEREST_RATE_MODE = 2; // Variable
+
+export const repayToAaveV3 = async (tokenAddress: string, atomicAmount: BigNumberish, receiver: string) => {
+  const operations = [];
+
+  const tokenInterface = new utils.Interface(tokenABI);
+
+  const approveData = tokenInterface.encodeFunctionData('approve', [AAVE_V3_POOL_CONTRACT_ADDRESS, atomicAmount]);
+  operations.push({
+    target: tokenAddress,
+    value: 0,
+    data: approveData
+  });
+
+  const aaveV3PoolInterface = new utils.Interface(aaveV3PoolABI);
+  const repayData = aaveV3PoolInterface.encodeFunctionData('repay', [
+    tokenAddress,
+    atomicAmount,
+    INTEREST_RATE_MODE,
+    receiver
+  ]);
+
+  operations.push({
+    target: AAVE_V3_POOL_CONTRACT_ADDRESS,
+    value: 0,
+    data: repayData
+  });
+
+  return operations;
+};
